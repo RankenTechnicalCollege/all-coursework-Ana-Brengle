3362124c65d8325a95283a66286f5e17cc6f1c58
Ana-Brengle <Mariana_Brengle@insideranken.org>
2025-11-17 16:16:31 -0600

    added code

---
+++ Labs/Labs/Typescript/Basics of type script/myArray.js
@@ -0,0 +1,10 @@
+var superHeros = [];
+//const heroPower: number[] = [];
+var heroPower = [];
+var allUsers = [];
+var MLModels = [
+    [255, 255, 255]
+];
+superHeros.push("Spiderman");
+heroPower.push(100);
+allUsers.push({ name: "Bruce", isActive: true });
