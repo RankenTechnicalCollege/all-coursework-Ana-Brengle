f837cb952543425c2cc89955bdc3fe3f1c17314b
Ana-Brengle <Mariana_Brengle@insideranken.org>
2025-11-18 16:20:13 -0600

    added code

---
+++ Labs/Labs/Tailwindcss/tailwindcss/src/App.jsx
@@ -0,0 +1,27 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div className='wrap-break p-5 bg-amber-800 space-y-4'>
+        <h1 className='text-green-500 line-through decoration-red-700 decoration-wavy'>Hello World</h1>
+        <h1 className='text-3xl'>Hello World</h1>
+        <h1 className='text-purple font-display'>Hello World</h1>
+        <h1 className='lowercase bg-yellow-50 py-2 m-3'>Hello World</h1>s
+        <h1 className='text-green bg-blue-300'>Hello World</h1>
+        <h1 className='border-6 border-solid'>Hello World</h1>
+        <h1 className=''>Hello World</h1>
+        <h1 className='uppercase text-blue-400 text-center'>Hello World</h1>
+        <h1 className='truncate'>Hello World!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!</h1>
+        <h1 className='text-xl'>Hello World</h1>
+      </div>
+      <div className='bg-cyan-400 min-h-3 w-1/3 '></div>
+      <div className='bg-emerald-500  mt-2 pt-6 h-15 w-5/6'></div>
+    </>
+  )
+}
+
+export default App
