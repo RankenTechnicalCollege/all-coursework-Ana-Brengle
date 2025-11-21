fd3bfa2ca850956cea5eb926f41dcd6e99503d74
Ana-Brengle <Mariana_Brengle@insideranken.org>
2025-11-21 16:17:56 -0600

    added code

--- Labs/Labs/lab5-01/shopping-cart/src/CartList.tsx
+++ Labs/Labs/lab5-01/shopping-cart/src/CartList.tsx
@@ -12,7 +12,7 @@ export default function CartList(){
     let itemCount = 0;
     for (const item of items) {
         if (item && item.quantity) {
-        itemCount += item.quantity
+            itemCount += item.quantity
         }
     }
 
@@ -26,10 +26,10 @@ export default function CartList(){
     function onAddQuantity(evt, item) {
         const newQuantity = item.quantity + 1;
         if (newQuantity <= 10) {
-        const newItems = [...items];
-        const index = items.indexOf(item);
-        newItems[index].quantity++;
-        setItems(newItems)
+            const newItems = [...items];
+            const index = items.indexOf(item);
+            newItems[index].quantity++;
+            setItems(newItems)
         }
         
     }
@@ -37,33 +37,40 @@ export default function CartList(){
     function onSubtractQuantity(evt, item) {
         const newQuantity = item.quantity - 1;
         if (newQuantity > 0) {
-        const newItems = [...items];
-        const index = items.indexOf(item);
-        newItems[index].quantity--;
-        setItems(newItems)
+            const newItems = [...items];
+            const index = items.indexOf(item);
+            newItems[index].quantity--;
+            setItems(newItems)
         }
         else {
-        setItems(items.filter(i => i.id !== item.id))
+            setItems(items.filter(i => i.id !== item.id))
         }
         
     }
     return(
         <>
-        <div className="container">
-            <span className="">Shopping Cart</span>
-            <span className="">{itemCount}</span>
-            <button className="" onClick={() => setItems({...items, {id: nanoid(), name: '', quantity:1}})}>Add Item</button>
+        <div className="flex flex-col p-4 border-4 border-blue-400 rounded-lg bg-stone-100">
+            <div className="flex font-bold text-3xl flex-items-center justify-between mb-5">
+                <span className="">Shopping Cart</span>
+                <span className="">{itemCount > 0 ? itemCount : 'Add Items To Cart'}</span>
+            <div className="flex mb-5">
+                <button className="px-4 py-2 mr-2 bg-blue-600 text-white rounded-full hover:bg-blue-700" onClick={() => setItems([...items, { id: nanoid(), name: '', quantity: 1 }])}>Add Item</button>
+                {items.length === 0 && (
+                    <div className="text-red-600 font-extrabold m-auto text-lg"> Your cart is empty! Add some items to it.</div>
+                )}
+            </div>
+            </div>
             {items.map(item => 
-                <div className="" key={item.id}>
-                    <div className="">
-                        <input type="text"  className="" value={item.name} onChange={(evt) => onNameChange(evt, item)}/>
+                <div className="grid grid-cols-3 mb-5" key={item.id}>
+                    <div className="col-span-1">
+                        <input type="text"  className={item.name.length > 0 ? "border-6 border-stone-900 rounded-sm px-1 bg-stone-50" : "border-5 border-red-400 rounded-sm px-1 bg-red-50"} value={item.name} onChange={(evt) => onNameChange(evt, item)}/>
                     </div>
                     <div className="">
                         <span className="">Qty: {item.quantity}</span>
                     </div>
                     <div className="">
-                        <button className="" onClick={() => onSubtractQuantity(evt, item)}>-</button>
-                        <button className="" onClick={() => onAddQuantity(evt, item)}>+</button>
+                        <button className="px-4 py-2 m-3 bg-blue-600 text-white rounded-full hover:bg-blue-700" disabled={item.quantity <=0 ? true:false} onClick={evt => onSubtractQuantity(evt, item)}>-</button>
+                        <button className="px-4 py-2 bg-red-600 text-white rounded-full hover:bg-red-700" disabled={item.quantity >=10 ? true:false} onClick={evt => onAddQuantity(evt, item)}>+</button>
                     </div>
                 </div>
             )}
