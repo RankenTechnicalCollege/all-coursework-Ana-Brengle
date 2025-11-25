2f9f144ee4c116cf67c5633dca9f0364c73594a2
Ana-Brengle <Mariana_Brengle@insideranken.org>
2025-11-25 16:18:51 -0600

    added code

--- FinalProject/IssueTracker/frontend/src/App.tsx
+++ FinalProject/IssueTracker/frontend/src/App.tsx
@@ -1,13 +1,27 @@
-import { Login1 } from '@/components/login1'
 import './App.css'
-
+import { LoginForm } from '@/components/login-form'
+import { authClient } from '@/lib/auth-client'
+import { Button } from './components/ui/button';
 
 function App() {
-
+  const {data: session, isPending} = authClient.useSession();
+  if(isPending) {
+    return <div>Loading......</div>
+  }
+  if(!session){
+    return (
+      <div className='flex min-h-svh items-center justify-center px-4'>
+        <div className='w-full max-w-md'>
+         < LoginForm/>
+        </div>
+      </div>
+    )
+  }
 
   return (
     <>
-      <Login1 />
+      <h1> Welcome, {session.user.email}</h1>
+      <Button variant="default" onClick={() => authClient.signOut }>Logout</Button>
     </>
   )
 }
